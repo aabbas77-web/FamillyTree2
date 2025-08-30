f9f12ecc6d738f1da41f30aa78c7d5c576e409e3
Ali Abbas <sami.2025.sami123654@gmail.com>
2025-08-30 10:43:24 +0000

    FirstRelease

--- FamillyTree2/ArabicLib.cpp
+++ FamillyTree2/ArabicLib.cpp
@@ -1,246 +1,81 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#include <Usp10.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+#include "ArabicLib.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+int GetArabicOffset(HDC hdc)
+{
+    AnsiString Text = 'Ú';// 0xFEC9
+	UINT Count = Text.Length();
+ 	wchar_t *Glyphs=new wchar_t[Count];
 
-><B
->HTTP_AUTH_DIGEST</B
-></TT
-> and/or <TT
-CLASS="constant"
-><B
->HTTP_AUTH_NTLM</B
-></TT
-></TD
-></TR
-><TR
-><TD
->compress</TD
-><TD
->bool</TD
-><TD
->whether to allow gzip/deflate content encoding</TD
-></TR
-><TR
-><TD
->port</TD
-><TD
->int</TD
-><TD
->use another port as specified in the url</TD
-></TR
-><TR
-><TD
->referer</TD
-><TD
->string</TD
-><TD
->the referer to send</TD
-></TR
-><TR
-><TD
->useragent</TD
-><TD
->string</TD
-><TD
->the user agent to send (defaults to PECL::HTTP/version (PHP/version))</TD
-></TR
-><TR
-><TD
->headers</TD
-><TD
->array</TD
-><TD
->list of custom headers as associative array like array("header" =&#62; "value")</TD
-></TR
-><TR
-><TD
->cookies</TD
-><TD
->array</TD
-><TD
->list of cookies as associative array like array("cookie" =&#62; "value")</TD
-></TR
-><TR
-><TD
->encodecookies</TD
-><TD
->bool</TD
-><TD
->whether to urlencode the cookies (default: true)</TD
-></TR
-><TR
-><TD
->cookiestore</TD
-><TD
->string</TD
-><TD
->path to a file where cookies are/will be stored</TD
-></TR
-><TR
-><TD
->cookiesession</TD
-><TD
->bool</TD
-><TD
->don't load session cookies from cookiestore if TRUE</TD
-></TR
-><TR
-><TD
->resume</TD
-><TD
->int</TD
-><TD
->byte offset to start the download from; if the server supports ranges</TD
-></TR
-><TR
-><TD
->range</TD
-><TD
->array</TD
-><TD
->array of arrays, each containing two integers, specifying the ranges to download if server support is given; only recognized if the resume option is empty</TD
-></TR
-><TR
-><TD
->maxfilesize</TD
-><TD
->int</TD
-><TD
->maximum file size that should be downloaded; has no effect, if the size of the requested entity is</TD
-></TR
-><TR
-><TD
->lastmodified</TD
-><TD
->int</TD
-><TD
->timestamp for If-(Un)Modified-Since header</TD
-></TR
-><TR
-><TD
->etag</TD
-><TD
->string</TD
-><TD
->quoted etag for If-(None-)Match header</TD
-></TR
-><TR
-><TD
->timeout</TD
-><TD
->int</TD
-><TD
->seconds the request may take</TD
-></TR
-><TR
-><TD
->connecttimeout</TD
-><TD
->int</TD
-><TD
->seconds the connect may take</TD
-></TR
-><TR
-><TD
->onprogress</TD
-><TD
->mixed</TD
-><TD
->progress callback</TD
-></TR
-><TR
-><TD
->interface</TD
-><TD
->string</TD
-><TD
->outgoing network interface (ifname, ip or hostname)</TD
-></TR
-><TR
-><TD
->portrange</TD
-><TD
->array</TD
-><TD
->2 integers specifying outgoing portrange to try</TD
-></TR
-><TR
-><TD
->ssl</TD
-><TD
->array</TD
-><TD
->&#13;        <P
->&#13;         <DIV
-CLASS="table"
-><A
-NAME="http.request.options.ssl"
-></A
-><P
-><B
->Table 2. with the following options:</B
-></P
-><TABLE
-BORDER="1"
-CLASS="CALSTABLE"
-><COL><COL><COL><THEAD
-><TR
-><TD
->Name</TD
-><TD
->Type</TD
-><TD
->Description</TD
-></TR
-></THEAD
-><TBODY
-><TR
-><TD
->cert</TD
-><TD
->string</TD
-><TD
->path to certificate</TD
-></TR
-><TR
-><TD
->certtype</TD
-><TD
->string</TD
-><TD
->type of certificate</TD
-></TR
-><TR
-><TD
->certpasswd</TD
-><TD
->string</TD
-><TD
->password for certificate</TD
-></TR
-><TR
-><TD
->key</TD
-><TD
->string</TD
-><TD
->path to key</TD
-></TR
-><TR
-><TD
->keytype</TD
-><TD
->string</TD
-><TD
->type of key</TD
-></TR
-><TR
-><TD
->keypasswd</TD
-><TD
->string</TD
-><TD
->pasword for key</TD
-></TR
-><TR
-><TD
->engine</TD
-><TD
->string</TD
+ 	ConvertToArabic(hdc,Text,Glyphs,NULL,NULL,Count);
+	int nOffset = 0xFEC9 - Glyphs[0];
+
+ 	if(Glyphs)	delete[] Glyphs;
+    return nOffset;
+}
+//---------------------------------------------------------------------------
+void ConvertToArabic(HDC hdc,AnsiString Text,UINT *Indices,UINT *Orders,int *pDx,UINT &Count)
+{
+	TEXTMETRIC TextMetrics;
+    GetTextMetrics(hdc,&TextMetrics);
+    UINT nOffset = 0;
+    switch(TextMetrics.tmCharSet)
+    {
+    	case ARABIC_CHARSET:
+        {
+        	nOffset = GetArabicOffset(hdc);
+         	break;
+        }
+    }
+
+	Count = Text.Length();
+ 	wchar_t *Glyphs=new wchar_t[Count];
+
+ 	ConvertToArabic(hdc,Text,Glyphs,Orders,pDx,Count);
+
+ 	for(UINT i=0;i<Count;i++)
+ 	{
+    	Indices[i] = nOffset + DWORD(Glyphs[i]);
+ 	}
+
+ 	if(Glyphs)	delete[] Glyphs;
+}
+//---------------------------------------------------------------------------
+void ConvertToArabic(HDC hdc,AnsiString Text,wchar_t *Glyphs,UINT *Orders,int *pDx,UINT &Count)
+{
+ 	GCP_RESULTS TextInfo;
+ 	DWORD Flag;
+    char OutString[255];
+
+// 	Flag = GetFontLanguageInfo(hdc);
+ 	Flag = GCP_REORDER;
+    Flag |= GCP_DIACRITIC;
+    Flag |= GCP_DISPLAYZWG;
+    Flag |= GCP_GLYPHSHAPE;
+    Flag |= GCP_JUSTIFY;
+    Flag |= GCP_KASHIDA;
+    Flag |= GCP_NEUTRALOVERRIDE;
+    Flag |= GCP_NUMERICSLATIN;
+    Flag |= GCP_NUMERICSLOCAL;
+    Flag |= GCP_USEKERNING;
+	Glyphs[0] = 0;
+
+ 	TextInfo.lStructSize=sizeof(GCP_RESULTS);
+ 	TextInfo.lpOutString=OutString;
+ 	TextInfo.lpOrder=Orders;
+ 	TextInfo.lpDx=pDx;
+ 	TextInfo.lpCaretPos=NULL;
+ 	TextInfo.lpClass=NULL;
+ 	TextInfo.lpGlyphs=Glyphs;
+ 	TextInfo.nGlyphs=Count;
+ 	TextInfo.nMaxFit=Count;
+ 	GetCharacterPlacement(hdc,Text.c_str(),Count,0,&TextInfo,Flag);
+}
+//---------------------------------------------------------------------------
+
